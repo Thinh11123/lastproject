bc36a71e06e556ecaf09cb57fbc50ee78918a628
unknown <thinh.nguyen281112@gmail.com>
2025-09-15 18:50:56 +0700

    commit

--- muasam/home/views.py
+++ muasam/home/views.py
@@ -21,8 +21,14 @@ from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "signup.html"
+    success_url = reverse_lazy("login")   # hoặc "index" nếu muốn về home
 def signup_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
